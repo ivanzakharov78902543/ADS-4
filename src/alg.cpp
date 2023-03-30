ebeb50757ce4282ef25264983b52859206b66aad
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-30 08:07:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,7 +4,7 @@ int countPairs1(int* arr, int len, int value) {
     for (int i = 0; i < len; i++) {
         for (int j = i + 1; j < len; j++) {
             if (arr[i] + arr[j] == value) {
-                count = count + 1;
+                count += 1;
             }
         }
     }
@@ -22,10 +22,10 @@ int countPairs2(int* arr, int len, int value) {
     return count;
 }
 int binsearch(int* arr, int l, int r, int value) {
+    int count = 0;
     int mid = 0;
     int k = 0;
-    int count = 0;
-    while (l <= r) {
+    while (r - l > 1) {
         mid = (l + r) / 2;
         if (arr[mid] == value) {
             count += 1;
@@ -41,18 +41,20 @@ int binsearch(int* arr, int l, int r, int value) {
             }
             break;
         }
+        if (arr[mid] < value) {
+            l = mid;
+        }
         if (arr[mid] > value) {
             r = mid;
-        } else {
-            l = mid;
         }
     }
     return count;
 }
 int countPairs3(int* arr, int len, int value) {
+    return 0;
     int count = 0;
     int val = 0;
-    for (int i = 0; i < len - 1; i++) {
+    for (int i = 0; i < len - 1; ++i) {
         val = value - arr[i];
         count += binsearch(arr, i, len, val);
     }
